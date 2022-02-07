9eaddf95528ea1042686ff97415070a904ac72ea
byalexcg <alexcg553@gmail.com>
2022-02-07 16:07:28 -0500

    add ejemplos

--- javascript/ejercicios.js
+++ javascript/ejercicios.js
@@ -6,10 +6,10 @@ var nombreAprendiz = "Jhon Alexander Castaño"
 console.log("Bienvenido(a) Señor(a) " + nombreAprendiz)
 /* Ej 3 */
 let costoProducto = 42000;
-let costoImpuesto = 4200;
+let costoImpuesto = 0.10;
 let domicilio = 2000;
-let subTotal = costoProducto + costoImpuesto;
-let costoTotal = costoProducto + costoImpuesto + domicilio;
+let subTotal = costoProducto + (costoProducto * costoImpuesto);
+let costoTotal = subTotal + domicilio;
 console.log("Su costo inicial es de: $"+ costoProducto);
 console.log("El costo sub total es de: $"+ subTotal)
 console.log("El costo total es de: $" + costoTotal);
