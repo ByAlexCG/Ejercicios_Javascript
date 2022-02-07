68dc47fa8b653572eed8a9d30062accd3acfc5bc
byalexcg <alexcg553@gmail.com>
2022-02-07 15:52:50 -0500

    add ejemplos

--- javascript/ejercicios.js
+++ javascript/ejercicios.js
@@ -4,3 +4,12 @@ console.log("Hola Mundo y Pan con Queso")
 /* Ej 2 */
 var nombreAprendiz = "Jhon Alexander Castaño"
 console.log("Bienvenido(a) Señor(a) " + nombreAprendiz)
+/* Ej 3 */
+let costoProducto = 42000;
+let costoImpuesto = 4200;
+let domicilio = 2000;
+let subTotal = costoProducto + costoImpuesto;
+let costoTotal = costoProducto + costoImpuesto + domicilio;
+console.log("Su costo inicial es de: $"+ costoProducto);
+console.log("El costo sub total es de: $"+ subTotal)
+console.log("El costo total es de: $" + costoTotal);
