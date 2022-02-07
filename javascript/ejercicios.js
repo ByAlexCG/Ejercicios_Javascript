130fd99e849e2078a0d057f9713a1e6ac5f85b37
byalexcg <alexcg553@gmail.com>
2022-02-07 14:50:42 -0500

    add ejemplos

---
+++ javascript/ejercicios.js
@@ -0,0 +1,6 @@
+"Use strict"
+/* Ej 1 */
+console.log("Hola Mundo y Pan con Queso")
+/* Ej 2 */
+var nombreAprendiz = "Jhon Alexander Castaño"
+console.log("Bienvenido(a) Señor(a) " + nombreAprendiz)
