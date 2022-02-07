09cfd57aa9afe1b976bee4583abc6bba82d61489
byalexcg <alexcg553@gmail.com>
2022-02-07 17:05:30 -0500

    add ejemplos

--- javascript/ejercicios.js
+++ javascript/ejercicios.js
@@ -13,3 +13,24 @@ let costoTotal = subTotal + domicilio;
 console.log("Su costo inicial es de: $"+ costoProducto);
 console.log("El costo sub total es de: $"+ subTotal)
 console.log("El costo total es de: $" + costoTotal);
+/* Ej 4 */
+let inicialCosto = 30000;
+let segundoCosto = 100000;
+let primerDescuento = 0.05;
+let segundoDescuento = 0.10;
+let descuentoDos = inicialCosto -(inicialCosto * 0.05);
+
+console.log("Su costo inicial es de $"+ inicialCosto)
+
+if (inicialCosto < 30000) {
+    console.log("No tiene descuento")
+}
+else if (inicialCosto >= 30000 && segundoCosto <= 100000) {
+    console.log("Tiene un descuento del 5%, Su costo es de $"+ descuentoDos)
+} 
+else if (segundoCosto >= 100000) {
+    console.log("Tiene un descuento del 10% ")
+} 
+else {
+    console.log("No tiene descuento")
+}
