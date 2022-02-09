044ce0b88c680ded17540cf754d2f469d334719e
byalexcg <alexcg553@gmail.com>
2022-02-09 13:41:45 -0500

    ej 5

--- javascript/ejercicios.js
+++ javascript/ejercicios.js
@@ -34,3 +34,4 @@ else if (segundoCosto >= 100000) {
 else {
     console.log("No tiene descuento")
 }
+
---
+++ javascript/ejercicios_arrays.js
@@ -0,0 +1,10 @@
+"Use strict"
+
+/* Ej 5 */
+let aprendiz = new Array ();
+aprendiz[0] = "Carolina";
+aprendiz[1] = "Liliana";
+aprendiz[2] = "Alex";
+aprendiz[3] = "Camilo";
+for(let i=0; i<=3; i++)
+console.log(aprendiz[i])
