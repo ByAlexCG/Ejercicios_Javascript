f090f8929a3b8f8018562d5d925e903da3d83669
Alex Castaño <alexcg553@gmail.com>
2022-02-21 11:13:51 -0500

    agg img

---
+++ javascript/manipulandoElementos.js
@@ -0,0 +1,9 @@
+function hacer_invicible(){
+    document.getElementById("uno").className = "hidden";
+}
+function intercambiar(){
+    document.getElementById("dos".src ="img/apex-legends-fuse.jpg")
+}
+function interConArg(id, ruta){
+    document.getElementById(id).src = ruta;
+}
